54bbeebaa82d0f76502486311fbbd2540c5c56b3
willeKoodaus <94905813+willeKoodaus@users.noreply.github.com>
2023-03-22 10:27:02 +0200

    Update Toimitusjohtaja.java

--- ChainOfResponsibility/src/Toimitusjohtaja.java
+++ ChainOfResponsibility/src/Toimitusjohtaja.java
@@ -3,7 +3,7 @@ public class Toimitusjohtaja extends Handler {
 	
 	public void handle(Double uusiPalkka) {
 		if ((uusiPalkka-duunarinNykyinenPalkka)/duunarinNykyinenPalkka >= 0.05) {
-			System.out.println("Toimitusjohtaja: Etko sina rahvas ymmarra, etta inflaatio pitaa saada alas niin ei voi korottaa palkkoja. \nJaetaan mielummin taas nama rahat ennatysbonuksina ja osinkoina. \nSanoin myos saunaillassa keskuspankin johtajalle, etta nostaa niita korkoja, etta varmasti tuntuu ostovoimassa. \nTuut painaa overia sitten yovuoroon jos tarviit lainanhoitoon ja ruokaan lisaa rahaa, opitpahan olemaan aiheuttamatta sita inflaatiota. \nJa turha yrittaa lakkoilla! Sanna oli kans saunomassa ja lupas, etta kaikki lakkoyritykset kielletaan taas lailla (@jauhojengi!)");
+			System.out.println("Toimitusjohtaja: Ei voida hyväksyä";)
 		} 
 		else {
 			super.handle(uusiPalkka);
