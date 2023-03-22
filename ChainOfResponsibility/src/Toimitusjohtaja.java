92d37cdb73d5ceec2cb100c8ce920fef1f77a6cc
willeKoodaus <94905813+willeKoodaus@users.noreply.github.com>
2023-03-22 10:27:35 +0200

    Update Toimitusjohtaja.java

--- ChainOfResponsibility/src/Toimitusjohtaja.java
+++ ChainOfResponsibility/src/Toimitusjohtaja.java
@@ -3,7 +3,7 @@ public class Toimitusjohtaja extends Handler {
 	
 	public void handle(Double uusiPalkka) {
 		if ((uusiPalkka-duunarinNykyinenPalkka)/duunarinNykyinenPalkka >= 0.05) {
-			System.out.println("Toimitusjohtaja: Ei voida hyväksyä";)
+			System.out.println("Toimitusjohtaja: Ei voida hyväksyä");
 		} 
 		else {
 			super.handle(uusiPalkka);
